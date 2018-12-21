b74e3778903ded97d3444f717fcfe5dce72c5525
Andrea Frola <base.mbr@gmail.com>
2018-12-21 16:10:47 +0100

    Angular update from 5.2 to 7.1
    Solved the issue, now the providedIn attribute is available.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,16 +14,17 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
-
   constructor(private heroService: HeroService) { }
+
   ngOnInit() {
     this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
